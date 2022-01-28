f51f9d0fea82fcd1161eb303148f290d598561d1
carviagu <85288542+carviagu@users.noreply.github.com>
2022-01-28 18:36:15 +0100

    Added ui structure

--- ui.R
+++ ui.R
@@ -2,5 +2,26 @@ library(shiny) # librería de shiny
 
 # interfaz grafica (frontend)
 shinyUI(
+  fluidPage(
+    
+    # Title of the app
+    titlePanel("Impact Effect Dashboard"),
+    
+    # Main panel
+    sidebarLayout(
+      
+      # Options section
+      sidebarPanel(
+        
+      ),
+      
+      # Plots area
+      mainPanel(
+        
+      )
+      
+    )
+    
+  )
 ) # UI
 
