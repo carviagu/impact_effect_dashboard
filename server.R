9c0269092e90d41bad4499300bbe704e8498f0ca
carviagu <85288542+carviagu@users.noreply.github.com>
2022-01-28 20:56:39 +0100

    Added navigation appearance

--- server.R
+++ server.R
@@ -1,11 +1,28 @@
 
 # Carga de datos
-
+library(tseries)
+cotizaciones <- get.hist.quote(instrument="san.mc", start= as.Date("2015-09-01"),
+                               end= as.Date("2017-03-01"), quote="AdjClose",
+                               provider="yahoo",
+                               compression="d", retclass="zoo")
 
 
 # Servidor (backend)
 shinyServer(
-  function(input, output) {
+  function(input, output, session) {
+    
+    # Welcome button functionality
+    observeEvent(input$goToSim, {
+      updateNavbarPage(session = session, inputId = "nav", selected = "Simulator")
+    })
+    
+    # Output
+    
+    output$mainPlot <- renderPlot({
+      #pre.period <- input$selDate
+      #post.val <- input$num
+    
+    })
     
   }
 )
--- ui.R
+++ ui.R
@@ -5,86 +5,128 @@ library(dplyr)
 
 # interfaz grafica (frontend)
 shinyUI(
-  fluidPage(
-    
-    # Title of the app
-    titlePanel("Impact Effect Dashboard"),
-    
-    # Main panel
-    sidebarLayout(
-    
-      # Options section
-      sidebarPanel(
-        p("Select an example dataset and stablish the start and end conditions of the impact, to
+  
+  # Navigation bar for the app
+  navbarPage("Impact Effect Dashboard",
+             id = "nav",
+             
+             # Welcome pane 
+             tabPanel("Welcome", fluid = TRUE,
+                      br(),
+                      h1(strong("Welcome to the Impact Effect Dashboard!"), 
+                         style = "font-size:50px;", align = "center"),
+                      p("Discover more about Causal Impact and see how it works", 
+                        style="font-size:25px;", align = "center"),
+                      div(style="display:inline-block",
+                          actionButton(inputId = "goToSim", label = "Start simulating", 
+                                       style="color: #fff; background-color: #337ab7; border-color: #2e6da4, float:center"), 
+                          style="float:center")
+                      ),
+             
+             # Simulation pane
+             tabPanel("Simulator", fluid = TRUE,
+               # Main panel
+               sidebarLayout(
+                 
+                 # Options section
+                 sidebarPanel(
+                   p("Select an example dataset and stablish the start and end conditions of the impact, to
           see Causal Impact in action:"),
-        
-        selectInput(
-          inputId = "selData",
-          label = h4('Dataset'),
-          choices = list(
-            "weather" = "weather",
-            "storms" = "storms",
-            "delay" = "delay"
-          ),
-          selected = "weather"
-        ),
-        
-        dateInput(
-          inputId = "selDate",
-          label = h4('Start Date'),
-          value = "2022-01-28"
-        ),
-        
-        br(),
-        h4("End condition"),
-        
-        selectInput(
-          inputId = "selEnd",
-          label = 'Criteria',
-          choices = list(
-            "Fixed Interval" = 0,
-            "Maximum" = 1,
-            "Minimum" = 2
-          ),
-          selected = "Fixed Interval"
-        ),
-        
-        conditionalPanel(
-          condition = "input.selEnd == 0",
-          numericInput(
-            inputId = "num",
-            label = 'Days',
-            value = 1
-          )
-        ),
-        
-        conditionalPanel(
-          condition = "input.selEnd == 1",
-          numericInput(
-            inputId = "num",
-            label = 'Upper limit',
-            value = 100
-          )
-        ),
-        
-        conditionalPanel(
-          condition = "input.selEnd == 2",
-          numericInput(
-            inputId = "num",
-            label = 'Lower limit',
-            value = -100
-          )
-        )
+                   
+                   selectInput(
+                     inputId = "selData",
+                     label = h4('Dataset'),
+                     choices = list(
+                       "weather" = "weather",
+                       "storms" = "storms",
+                       "delay" = "delay"
+                     ),
+                     selected = "weather"
+                   ),
+                   
+                   dateInput(
+                     inputId = "selDate",
+                     label = h4('Start Date'),
+                     value = "2022-01-28"
+                   ),
+                   
+                   br(),
+                   h4("End condition"),
+                   
+                   selectInput(
+                     inputId = "selEnd",
+                     label = 'Criteria',
+                     choices = list(
+                       "Fixed Interval" = 0,
+                       "Maximum" = 1,
+                       "Minimum" = 2
+                     ),
+                     selected = "Fixed Interval"
+                   ),
+                   
+                   conditionalPanel(
+                     condition = "input.selEnd == 0",
+                     numericInput(
+                       inputId = "num",
+                       label = 'Days',
+                       value = 1
+                     )
+                   ),
+                   
+                   conditionalPanel(
+                     condition = "input.selEnd == 1",
+                     numericInput(
+                       inputId = "num",
+                       label = 'Upper limit',
+                       value = 100
+                     )
+                   ),
+                   
+                   conditionalPanel(
+                     condition = "input.selEnd == 2",
+                     numericInput(
+                       inputId = "num",
+                       label = 'Lower limit',
+                       value = -100
+                     )
+                   )
+                   
+                 ),
+                 
+                 # Plots area
+                 mainPanel(
+                   plotOutput(
+                     outputId = 'mainPlot'
+                   ),
+                   
+                   h4('More details:'),
+                   
+                   checkboxGroupInput(
+                     inputId = 'inpuCheck',
+                     labe = "",
+                     choices = list(
+                       "Accumulated impact" = 1,
+                       "Punctual impact" = 2,
+                       "Relative impact" = 3
+                     ),
+                     selected = 1
+                   ),
+                   
+                   plotOutput(
+                     outputId = 'subPlot'
+                   )
+                 )
+                 
+               )
         
       ),
       
-      # Plots area
-      mainPanel(
+      tabPanel("About"
         
       )
       
     )
+   
     
-  )
 ) # UI
 
